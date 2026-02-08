c1155b74b86a0c02731d59724444e0b8db470b0a
Alistair Brown <github@alistairjcbrown.com>
2026-02-08 22:22:46 +0000

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,34 @@
+import { type MetadataRoute } from "next";
+import slugify from "@sindresorhus/slugify";
+import { getStaticData } from "@/utils/get-static-data";
+import { Movie } from "@/types";
+
+export const dynamic = "force-static";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const data = await getStaticData();
+
+  const moviePages = Object.values(data.movies).map((movie: Movie) => ({
+    url: `https://clusterflick.com/movies/${movie.id}/${slugify(movie.title)}`,
+    lastModified: data.generatedAt,
+    changeFrequency: "daily" as const,
+    priority: 0.7,
+  }));
+
+  const staticPages = [
+    {
+      url: "https://clusterflick.com",
+      lastModified: data.generatedAt,
+      changeFrequency: "daily" as const,
+      priority: 1,
+    },
+    {
+      url: "https://clusterflick.com/about",
+      lastModified: data.generatedAt,
+      changeFrequency: "weekly" as const,
+      priority: 0.8,
+    },
+  ];
+
+  return [...staticPages, ...moviePages];
+}
