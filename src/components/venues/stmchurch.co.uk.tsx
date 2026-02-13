3e9d53d327bd9a485b65d06fc691564d355782fe
Alistair Brown <github@alistairjcbrown.com>
2026-02-13 15:35:27 +0000

    Add one venue

---
+++ src/components/venues/stmchurch.co.uk.tsx
@@ -0,0 +1,21 @@
+function VenueBlurb() {
+  return (
+    <section>
+      <p>
+        St Matthias Church is a Grade I-listed Anglican church on Wordsworth
+        Road in Stoke Newington, the only church in the area with that
+        designation. The Victorian building seats up to 250 with flexible
+        seating and excellent acoustics, and the adjoining St Matthias Hall
+        offers a more intimate space for up to 100. The church has also served
+        as a filming location, notably for the opening episode of Killing Eve
+        series 4.
+      </p>
+    </section>
+  );
+}
+
+export const seoDescription =
+  "Grade I-listed church and community venue in Stoke Newington";
+export const seoHighlights = "community screenings and events";
+
+export default VenueBlurb;
