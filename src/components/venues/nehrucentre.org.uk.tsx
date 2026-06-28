ad3ae65d186bce019808e59f5f34f15fc002d54a
Alistair Brown <github@alistairjcbrown.com>
2026-06-29 00:46:22 +0100

    Add new venue

---
+++ src/components/venues/nehrucentre.org.uk.tsx
@@ -0,0 +1,22 @@
+function VenueBlurb() {
+  return (
+    <section>
+      <p>
+        The Nehru Centre is the cultural wing of the High Commission of India in
+        the UK, located on South Audley Street in Mayfair. It promotes Indian
+        arts and culture in Britain through a programme of classical and
+        contemporary dance, music, exhibitions, talks, and film.
+      </p>
+      <p>
+        Its events bring together Indian and British audiences, with screenings
+        and cultural showcases that offer a window onto Indian cinema and
+        heritage in the heart of central London.
+      </p>
+    </section>
+  );
+}
+
+export const seoDescription = "Mayfair Indian cultural centre and venue";
+export const seoHighlights = "Indian cinema, cultural showcases and talks";
+
+export default VenueBlurb;
